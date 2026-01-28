4b8136c740920a0fb03f8cc8ed29d105f9d04833
quyenha1225 <quyenha419@gmail.com>
2026-01-29 00:04:44 +0700

    Refactor MessageService interface package structure

--- BIT242958_HaVanVoQuyen/Chapter_2/Chapter_2/src/main/java/Chapter_2/Bai_5/MessageService.java
+++ BIT242958_HaVanVoQuyen/Chapter_2/Chapter_2/src/main/java/Chapter_2/Bai_5/MessageService.java
@@ -2,8 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package Chapter_2;
-
+package Chapter_2.Bai_5;
 /**
  *
  * @author Asus
