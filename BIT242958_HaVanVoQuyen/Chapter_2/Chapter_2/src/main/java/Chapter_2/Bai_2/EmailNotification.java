272fcb4bed9c59807aa49b1398540418b87a962b
quyenha1225 <quyenha419@gmail.com>
2026-01-29 00:02:34 +0700

    Update EmailNotification.java

--- BIT242958_HaVanVoQuyen/Chapter_2/Chapter_2/src/main/java/Chapter_2/Bai_2/EmailNotification.java
+++ BIT242958_HaVanVoQuyen/Chapter_2/Chapter_2/src/main/java/Chapter_2/Bai_2/EmailNotification.java
@@ -2,15 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Chapter_2;
-
+package Chapter_2.Bai_5;
 /**
  *
  * @author Asus
  */
-public class EmailNotification implements Notification{
-    public void notifyUser(){
-        System.out.print("Gửi Push Notification");
+public class EmailService implements MessageService {
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Gửi Email: " + message);
     }
- 
 }
